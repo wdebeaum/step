be25e58f210e0909a5551a827a2edd3dee8ab901
William de Beaumont <wdebeaum@cs.rochester.edu>
2016-06-04 01:42:38 -0400

    ../update-git-mirror-of-cvs-module.rb

--- src/LexiconManager/Data/new/transmute.lisp
+++ src/LexiconManager/Data/new/transmute.lisp
@@ -8,7 +8,7 @@
    (SENSES
     ((meta-data :origin "verbnet-2.0-corrected" :entry-date 20060315 :change-date nil :comments nil :vn ("turn-26.6.1") :wn ("transmute%2:30:00" "transmute%2:30:01" "transmute%2:30:02"))
      (LF-PARENT ONT::adjust)
-     (TEMPL agent-theme-result-optional-templ (xp (% w::pp (w::ptype w::into)))) ; like translate but pp is into not to
+     (TEMPL agent-theme-affected-optional-templ (xp (% w::pp (w::ptype w::into)))) ; like translate but pp is into not to
      )
     )
    )
