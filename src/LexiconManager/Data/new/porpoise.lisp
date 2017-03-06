3424db510c595257f6f66def30fa586ede153ca7
William de Beaumont <wdebeaum@cs.rochester.edu>
2017-03-06 01:42:27 -0500

    ../update-git-mirror-of-cvs-module.rb

--- src/LexiconManager/Data/new/porpoise.lisp
+++ src/LexiconManager/Data/new/porpoise.lisp
@@ -1,10 +1,10 @@
 ;;;;
-;;;; w::porpise
+;;;; w::porpoise
 ;;;;
 
 (define-words :pos w::N 
  :words (
-  (w::porpise
+  (w::porpoise
   (senses((LF-parent ONT::nonhuman-animal) 
 	    (templ count-pred-templ)
 	    (meta-data :origin calo-ontology :entry-date 20060128 :change-date nil :comments caloy3)
