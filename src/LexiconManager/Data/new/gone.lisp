2ddac409a6bbee90dab5c045b2c57a109a6cd945
William de Beaumont <wdebeaum@cs.rochester.edu>
2019-01-08 01:41:36 -0500

    ../update-git-mirror-of-cvs-module.rb

---
+++ src/LexiconManager/Data/new/gone.lisp
@@ -0,0 +1,16 @@
+;;;;
+;;;; w::gone
+;;;; 
+
+(define-words :pos W::adj
+ :tags (:base500)
+ :words (
+  (W::gone
+   (SENSES
+    ((LF-PARENT ONT::NOT-AVAILABLE-VAL)
+     (TEMPL predicative-only-adj-templ)
+     (example "the book is gone")
+     )
+    )
+   )
+))
