c8e69f703f5e48c312d95a1001a2dca20db53374
William de Beaumont <wdebeaum@cs.rochester.edu>
2019-06-20 01:41:23 -0400

    ../update-git-mirror-of-cvs-module.rb

--- src/LexiconManager/Data/new/punc-ordinal.lisp
+++ src/LexiconManager/Data/new/punc-ordinal.lisp
@@ -10,5 +10,48 @@
      (non-hierarchy-lf t))
     )
    )
+  ))
+
+; 1st
+(define-words :pos W::punc :boost-word t :templ NO-FEATURES-TEMPL
+ :words (
+  (W::st
+   (SENSES
+    ((LF (W::ORDINAL))
+     (non-hierarchy-lf t))
+    )
+   )
 ))
+
+; 2nd
+(define-words :pos W::punc :boost-word t :templ NO-FEATURES-TEMPL
+ :words (
+  (W::nd
+   (SENSES
+    ((LF (W::ORDINAL))
+     (non-hierarchy-lf t))
+    )
+   )
+))
+
+(define-words :pos W::punc :boost-word t :templ NO-FEATURES-TEMPL
+ :words (
+  (W::rd
+   (SENSES
+    ((LF (W::ORDINAL))
+     (non-hierarchy-lf t))
+    )
+   )
+))
+
+(define-words :pos W::punc :boost-word t :templ NO-FEATURES-TEMPL
+ :words (
+  (W::th
+   (SENSES
+    ((LF (W::ORDINAL))
+     (non-hierarchy-lf t))
+    )
+   )
+))
+
 
