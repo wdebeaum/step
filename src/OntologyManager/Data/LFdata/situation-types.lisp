62589c59e6433f698f4c7291117b0815ac7e6487
William de Beaumont <wdebeaum@cs.rochester.edu>
2020-03-28 01:41:45 -0400

    ../update-git-mirror-of-cvs-module.rb

--- src/OntologyManager/Data/LFdata/situation-types.lisp
+++ src/OntologyManager/Data/LFdata/situation-types.lisp
@@ -523,7 +523,7 @@
  )
 
 (define-type ONT::Experiencer-emotion
- :wordnet-sense-keys ( "experience%2:37:00" "feel%2:37:00")
+ :wordnet-sense-keys ( "experience%2:37:00" "feel%2:37:00" "feel%2:29:00")
  :parent ONT::event-of-experience
  :sem (F::Situation (:required (F::Cause F::Mental))(:default (F::Aspect F::Stage-Level)))
  :arguments (
