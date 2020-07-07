47f1e54cb9912b11c4ba74b8e2d3bfcf23dea557
William de Beaumont <wdebeaum@cs.rochester.edu>
2020-07-07 01:41:51 -0400

    ../update-git-mirror-of-cvs-module.rb

---
+++ src/LexiconManager/Data/new/metallic.lisp
@@ -0,0 +1,13 @@
+;;;;
+;;;; W::METALLIC
+;;;;
+
+(define-words :pos W::adj :templ CENTRAL-ADJ-TEMPL
+ :words (
+  (W::METALLIC
+   (SENSES
+     ((LF-PARENT ONT::containing-substance-val)
+     )
+    )
+   )
+))
---
+++ src/LexiconManager/Data/new/nonmetallic.lisp
@@ -0,0 +1,13 @@
+;;;;
+;;;; W::NONMETALLIC
+;;;;
+
+(define-words :pos W::adj :templ CENTRAL-ADJ-TEMPL ;Could not find a lexical item that could go under ont::not-containing-substance-val that did not consist of an affix, so for now, created this lexical item with prefix non-.
+ :words (
+  (W::NONMETALLIC
+   (SENSES
+     ((LF-PARENT ONT::not-containing-substance-val)
+     )
+    )
+   )
+))
